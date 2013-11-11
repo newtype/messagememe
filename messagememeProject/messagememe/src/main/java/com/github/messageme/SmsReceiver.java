7f0917a9631340cf6d1cde541994ec2a74f7b649
Keith Trnka <trnka.dev@gmail.com>
2013-11-10 21:19:54 -0800

    Cleanpup
    Little bit of rearranging.  If LOG_SMS_ONLY is enabled, it won't
    actually send SMS.  If contact isn't found (such as for automated
    messages), it won't notify.

--- messagememeProject/messagememe/src/main/java/com/github/messageme/SmsReceiver.java
+++ messagememeProject/messagememe/src/main/java/com/github/messageme/SmsReceiver.java
@@ -5,6 +5,7 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
@@ -28,7 +29,7 @@ import java.io.InputStream;
  */
 public class SmsReceiver extends BroadcastReceiver {
 
-    public static final String TAG = "SmsReceiver";
+    private static final String TAG = "SmsReceiver";
     private static final String AUTO_RESPONSE_INTENT = "com.github.messageme.AUTO_RESPONSE";
     private static final String DESTINATION_ADDRESS = "destinationAddress";
     private static final String BODY = "body";
@@ -36,6 +37,7 @@ public class SmsReceiver extends BroadcastReceiver {
     private static final int PENDING_NEGATIVE = 1;
     private static final String NOTIFICATION_ID = "notificationId";
     private static final int PENDING_TIME = 2;
+    private static final boolean LOG_SMS_ONLY = false;
     private static int currentNotificationId = 0;
 
     @Override
@@ -50,10 +52,10 @@ public class SmsReceiver extends BroadcastReceiver {
     }
 
     private void handleIncomingSms(Context context, Intent intent) {
-        final Bundle bundle = intent.getExtras();
+        Log.v(TAG, "handleIncomingSms");
 
+        final Bundle bundle = intent.getExtras();
         try {
-            Log.v(TAG, "handleIncomingSms");
             if (bundle != null) {
 
                 final Object[] pdusObj = (Object[]) bundle.get("pdus");
@@ -80,9 +82,12 @@ public class SmsReceiver extends BroadcastReceiver {
 
         Log.v(TAG, "Send \"" + body + "\" to " + destination);
 
-        // send a text message without an intent to notify that it's been sent
-        //sms.sendTextMessage(destination, null, body, null, null);
-        Toast.makeText(context, "Fake send to " + destination, Toast.LENGTH_LONG).show();
+        if (LOG_SMS_ONLY) {
+            Toast.makeText(context, "Fake send to " + destination, Toast.LENGTH_LONG).show();
+        }
+        else {
+            SmsManager.getDefault().sendTextMessage(destination, null, body, null, null);
+        }
 
         // clear the notification
         NotificationManager notificationManager =
@@ -95,9 +100,14 @@ public class SmsReceiver extends BroadcastReceiver {
         String phoneNumber = currentMessage.getDisplayOriginatingAddress();
 
         String smsMessage = currentMessage.getDisplayMessageBody();
-
         String contactName = getContactNameFromPhoneNumber(context, phoneNumber);
 
+        if (contactName == null) {
+            Log.v(TAG, "No contact name for " + phoneNumber + ", not showing popup");
+            return;
+        }
+
+        // TODO: This code should load the quick response configuration from somewhere and build more programatically
         Intent positiveReplyIntent = buildQuickResponseIntent(context, phoneNumber, context.getString(R.string.response_yes), currentNotificationId);
         PendingIntent positivePending = PendingIntent.getBroadcast(context, PENDING_POSITIVE, positiveReplyIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
         
@@ -110,13 +120,13 @@ public class SmsReceiver extends BroadcastReceiver {
         // TODO: addAction is Jelly Bean and above.  Switch to NotificationCompat or require JB API level.
         Notification.Builder builder = new Notification.Builder(context)
                 .setSmallIcon(R.drawable.ic_launcher)
-                .setContentTitle("Message from " + contactName)
+                .setContentTitle(contactName)
                 .setContentText(smsMessage)
                 .addAction(android.R.drawable.ic_media_play, context.getString(R.string.response_yes), positivePending)
                 .addAction(android.R.drawable.ic_menu_recent_history, context.getString(R.string.response_time), timePending)
                 .addAction(android.R.drawable.ic_menu_close_clear_cancel, context.getString(R.string.response_no), negativePending);
 
-        Bitmap contactPhoto = getContactPhoto(context, phoneNumber);
+        Bitmap contactPhoto = getContactPhoto(context.getContentResolver(), getContactThumbnailId(context.getContentResolver(), phoneNumber));
         if (contactPhoto != null) {
             builder.setLargeIcon(contactPhoto);
         }
@@ -142,43 +152,47 @@ public class SmsReceiver extends BroadcastReceiver {
         return null;
     }
 
-    private Bitmap getContactPhoto(Context context, String phoneNumber) {
-        // step 1: look up the thumbnail id
+    private int getContactThumbnailId(ContentResolver resolver, String phoneNumber) {
         final Uri uri = Uri.withAppendedPath(ContactsContract.CommonDataKinds.Phone.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
-        final Cursor cursor = context.getContentResolver().query(uri, new String[]{ContactsContract.Contacts.PHOTO_ID}, null, null, ContactsContract.Contacts.DISPLAY_NAME + " ASC");
+        final Cursor cursor = resolver.query(uri, new String[]{ContactsContract.Contacts.PHOTO_ID}, null, null, ContactsContract.Contacts.DISPLAY_NAME + " ASC");
+        int thumbnailId = 0;
 
         try {
-            int thumbnailId = 0;
             if (cursor.moveToFirst()) {
                 thumbnailId = cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts.PHOTO_ID));
             }
-            else {
-                return null;
-            }
-
-            // step 2: load the photo
-            final Uri photoUri = ContentUris.withAppendedId(ContactsContract.Data.CONTENT_URI, thumbnailId);
-            final Cursor photoCursor = context.getContentResolver().query(photoUri, new String[]{ContactsContract.CommonDataKinds.Photo.PHOTO}, null, null, null);
-
-            try {
-                Bitmap thumbnail = null;
-                if (photoCursor.moveToFirst()) {
-                    final byte[] thumbnailBytes = photoCursor.getBlob(0);
-                    if (thumbnailBytes != null) {
-                        thumbnail = BitmapFactory.decodeByteArray(thumbnailBytes, 0, thumbnailBytes.length);
-                        Log.v(TAG, "Loaded image of size " + thumbnail.getWidth() + " x " + thumbnail.getHeight());
-                    }
-                }
-                return thumbnail;
-            }
-            finally {
-                photoCursor.close();
-            }
-
         }
         finally {
             cursor.close();
         }
+
+        return thumbnailId;
+    }
+
+    private Bitmap getContactPhoto(ContentResolver resolver, int thumbnailId) {
+        if (thumbnailId == 0) {
+            return null;
+        }
+
+        final Uri uri = ContentUris.withAppendedId(ContactsContract.Data.CONTENT_URI, thumbnailId);
+        final Cursor cursor = resolver.query(uri, new String[]{ContactsContract.CommonDataKinds.Photo.PHOTO}, null, null, null);
+
+        Bitmap thumbnail = null;
+
+        try {
+            if (cursor.moveToFirst()) {
+                final byte[] thumbnailBytes = cursor.getBlob(0);
+                if (thumbnailBytes != null) {
+                    thumbnail = BitmapFactory.decodeByteArray(thumbnailBytes, 0, thumbnailBytes.length);
+                    Log.v(TAG, "Loaded image of size " + thumbnail.getWidth() + " x " + thumbnail.getHeight());
+                }
+            }
+        }
+        finally {
+            cursor.close();
+        }
+
+        return thumbnail;
     }
 
     private Intent buildQuickResponseIntent(Context context, String destinationAddress, String body, int notificationId) {
