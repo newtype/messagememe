8d2267e614eaccec86c5eb050d1fd0d6896d832a
Keith Trnka <trnka.dev@gmail.com>
2013-11-10 20:02:36 -0800

    Mostly working contact photo
    Added contact photo, but haven't tested it much.  Also, there's
    superfluous margins around the contact photo that seem to be a part of
    the default layout.

--- messagememeProject/messagememe/src/main/java/com/github/messageme/SmsReceiver.java
+++ messagememeProject/messagememe/src/main/java/com/github/messageme/SmsReceiver.java
@@ -5,9 +5,12 @@ import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
@@ -18,6 +21,7 @@ import android.util.Log;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * Created by ryan on 11/9/13.
@@ -104,7 +108,7 @@ public class SmsReceiver extends BroadcastReceiver {
         PendingIntent negativePending = PendingIntent.getBroadcast(context, PENDING_NEGATIVE, negativeReplyIntent, Intent.FLAG_ACTIVITY_NEW_TASK);
 
         // TODO: addAction is Jelly Bean and above.  Switch to NotificationCompat or require JB API level.
-        Notification.Builder mBuilder = new Notification.Builder(context)
+        Notification.Builder builder = new Notification.Builder(context)
                 .setSmallIcon(R.drawable.ic_launcher)
                 .setContentTitle("Message from " + contactName)
                 .setContentText(smsMessage)
@@ -112,10 +116,15 @@ public class SmsReceiver extends BroadcastReceiver {
                 .addAction(android.R.drawable.ic_menu_recent_history, context.getString(R.string.response_time), timePending)
                 .addAction(android.R.drawable.ic_menu_close_clear_cancel, context.getString(R.string.response_no), negativePending);
 
-        NotificationManager mNotificationManager =
+        Bitmap contactPhoto = getContactPhoto(context, phoneNumber);
+        if (contactPhoto != null) {
+            builder.setLargeIcon(contactPhoto);
+        }
+
+        NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-        mNotificationManager.notify(currentNotificationId, mBuilder.build());
+        notificationManager.notify(currentNotificationId, builder.build());
 
         currentNotificationId++;
     }
@@ -131,6 +140,44 @@ public class SmsReceiver extends BroadcastReceiver {
         }
 
         return null;
+    }
+
+    private Bitmap getContactPhoto(Context context, String phoneNumber) {
+        // step 1: look up the thumbnail id
+        final Uri uri = Uri.withAppendedPath(ContactsContract.CommonDataKinds.Phone.CONTENT_FILTER_URI, Uri.encode(phoneNumber));
+        final Cursor cursor = context.getContentResolver().query(uri, new String[]{ContactsContract.Contacts.PHOTO_ID}, null, null, ContactsContract.Contacts.DISPLAY_NAME + " ASC");
+
+        try {
+            int thumbnailId = 0;
+            if (cursor.moveToFirst()) {
+                thumbnailId = cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts.PHOTO_ID));
+            }
+            else {
+                return null;
+            }
+
+            // step 2: load the photo
+            final Uri photoUri = ContentUris.withAppendedId(ContactsContract.Data.CONTENT_URI, thumbnailId);
+            final Cursor photoCursor = context.getContentResolver().query(photoUri, new String[]{ContactsContract.CommonDataKinds.Photo.PHOTO}, null, null, null);
+
+            try {
+                Bitmap thumbnail = null;
+                if (photoCursor.moveToFirst()) {
+                    final byte[] thumbnailBytes = photoCursor.getBlob(0);
+                    if (thumbnailBytes != null) {
+                        thumbnail = BitmapFactory.decodeByteArray(thumbnailBytes, 0, thumbnailBytes.length);
+                    }
+                }
+                return thumbnail;
+            }
+            finally {
+                photoCursor.close();
+            }
+
+        }
+        finally {
+            cursor.close();
+        }
     }
 
     private Intent buildQuickResponseIntent(Context context, String destinationAddress, String body, int notificationId) {
